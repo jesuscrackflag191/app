30ae57304c8eb87a674346b84085b43d10e75dba
jesuscrackflag191 <jesusmolinarodrigue26@gmail.com>
2020-12-05 15:49:55 -0300

    tokenquitado

--- index.js
+++ index.js
@@ -79,26 +79,6 @@ client.on('message', message =>{
 });
  
 
-client.on("message", async(message) => {
-    let prefix;
-    if(prefix_db.tiene(`${message.guild.id}`)) {
-        prefix = await prefix_db.obtener(`${message.guild.id}`)
-    }else{
-        prefix = "r!"
-    }
-
-    if (!message.content.startsWith(prefix)) return;
-
-    let args = message.content.slice(prefix.length).trim().split(/ +/g)
-    let command = args.shift().toLowerCase();
-    
-    if(command == "setprefix") {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No tienes permisos para hacer esto")
-    if(!args[0]) return message.channel.send("Nesecitas colocar el prefix del servidor")
-    prefix_db.agregar(`${message.guild.id}`, args[0])
-    return message.channel.send("El prefix cambio a: "+args[0])
-    }
-});
 
 client.on('message', msg => {
     if (msg.author == client.user){return}
