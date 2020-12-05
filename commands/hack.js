49b3b49897af23e5f450f57fa6bcb646ac1d3005
jesuscrackflag191 <jesusmolinarodrigue26@gmail.com>
2020-12-05 16:44:11 -0300

    tokenquitado

--- commands/hack.js
+++ commands/hack.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 
 module.exports = {
-    name: "ejemplo",
+    name: "hack",
     description: "Este es el ejemplo para no escribir",
     execute(message, args){
 
